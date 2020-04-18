ba6003bba78ca2ddf91e68316f688cf41c2cfd65
ernestocuesy <ercuesy@gmail.com>
2020-04-18 12:31:22 +0200

    New methods for notifications and authorizations. Cleanup needed.

--- lib/app/services/database.dart
+++ lib/app/services/database.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'package:meta/meta.dart';
+import 'package:nearbymenus/app/models/authorizations.dart';
+import 'package:nearbymenus/app/models/user_notification.dart';
 import 'package:nearbymenus/app/models/job.dart';
 import 'package:nearbymenus/app/models/entry.dart';
 import 'package:nearbymenus/app/models/restaurant.dart';
@@ -14,18 +16,22 @@ abstract class Database {
   Future<void> deleteJob(Job job);
   Stream<List<Job>> jobsStream();
   Stream<Job> jobStream({@required String jobId});
-
   Future<void> setEntry(Entry entry);
   Future<void> deleteEntry(Entry entry);
   Stream<List<Entry>> entriesStream({Job job});
+  // TODO cleanup unused above definitions
   Future<void> setUserDetails(UserDetails userDetails);
   Stream<UserDetails> userDetailsStream();
   Stream<List<Restaurant>> managerRestaurants(String restaurantId);
   Stream<List<Restaurant>> userRestaurant(String restaurantId);
-  Stream<List<Restaurant>> restaurantsStream(String restaurantId);
   Future<List<Restaurant>> patronRestaurants();
-  Stream<Restaurant> restaurantStream();
   Future<void> setRestaurantDetails(Restaurant restaurant);
+  Future<void> setNotificationDetails(UserNotification roleNotification);
+  Stream<UserNotification> userNotifications(String uid);
+  //Stream<List<UserNotification>> userNotifications2(String uId);
+  Stream<List<UserNotification>> userNotifications2(String restaurantId, String uid, String toRole);
+  Stream<Authorizations> authorizationsStream(String restaurantId);
+  Future<void> setAuthorization(String restaurantId, Authorizations authorizations);
 }
 
 String documentIdFromCurrentDate() => DateTime.now().toIso8601String();
@@ -95,15 +101,24 @@ class FirestoreDatabase implements Database {
         sort: (lhs, rhs) => rhs.start.compareTo(lhs.start),
       );
 
+  // Used by several widgets
   @override
   Future<void> setUserDetails(UserDetails userDetails) async => await _service
       .setData(path: APIPath.userDetails(uid), data: userDetails.toMap());
 
+  // Used by session control
   @override
   Stream<UserDetails> userDetailsStream() => _service.documentStream(
         path: APIPath.userDetails(uid),
         builder: (data, documentId) => UserDetails.fromMap(data),
       );
+
+  // Used by staff authorizations
+  @override
+  Stream<Authorizations> authorizationsStream(String restaurantId) => _service.documentStream(
+    path: APIPath.authorization(restaurantId),
+    builder: (data, documentId) => Authorizations.fromMap(data, documentId),
+  );
 
   // Used by restaurant details page
   @override
@@ -115,6 +130,16 @@ class FirestoreDatabase implements Database {
     builder: (data, documentId) => Restaurant.fromMap(data, documentId),
   );
 
+  // Used in notifications page
+//  @override
+//  Stream<List<UserNotification>> userNotifications2(String uId) => _service.collectionStream(
+//    path: APIPath.notifications(),
+//    queryBuilder: uId != null
+//        ? (query) => query.where('toUid', isEqualTo: uId)
+//        : null,
+//    builder: (data, documentId) => UserNotification.fromMap(data, documentId),
+//  );
+
   // Used by account page to load user's (patron or staff) restaurant
   @override
   Stream<List<Restaurant>> userRestaurant(String restaurantId) => _service.collectionStream(
@@ -123,6 +148,25 @@ class FirestoreDatabase implements Database {
         ? (query) => query.where('id', isEqualTo: restaurantId)
         : null,
     builder: (data, documentId) => Restaurant.fromMap(data, documentId),
+  );
+
+  // User notifications 2 - in progress
+  @override
+  Stream<List<UserNotification>> userNotifications2(String restaurantId, String uid, String toRole) => _service.collectionStream(
+    path: APIPath.notifications(),
+    queryBuilder: restaurantId != null
+        ? (query) => query.where('restaurantId', isEqualTo: restaurantId)
+                          .where('toUid', isEqualTo: uid)
+                          .where('toRole', isEqualTo: toRole)
+        : null,
+    builder: (data, documentId) => UserNotification.fromMap(data, documentId),
+  );
+
+  // Used to listen to notifications sent to user
+  @override
+  Stream<UserNotification> userNotifications(String uid) => _service.documentStream(
+    path: APIPath.notification(uid),
+    builder: (data, documentId) => UserNotification.fromMap(data, documentId),
   );
 
   // Used by nearest restaurant query
@@ -137,18 +181,14 @@ class FirestoreDatabase implements Database {
   Future<void> setRestaurantDetails(Restaurant restaurant) async => await _service
       .setData(path: APIPath.restaurant(restaurant.id), data: restaurant.toMap());
 
-  // Not used
+  // Used when saving notification details
   @override
-  Stream<List<Restaurant>> restaurantsStream(String restaurantId) => _service.collectionStream(
-    path: APIPath.restaurants(),
-    builder: (data, documentId) => Restaurant.fromMap(data, documentId),
-  );
+  Future<void> setNotificationDetails(UserNotification roleNotification) async => await _service
+      .setData(path: APIPath.notification(roleNotification.id), data: roleNotification.toMap());
 
-  // Not used
+  // Used when saving authorizations
   @override
-  Stream<Restaurant> restaurantStream() => _service.documentStream(
-    path: APIPath.restaurants(),
-    builder: (data, documentId) => Restaurant.fromMap(data, documentId),
-  );
+  Future<void> setAuthorization(String restaurantId, Authorizations authorizations) async => await _service
+      .setData(path: APIPath.authorization(restaurantId), data: authorizations.toMap());
 
 }
