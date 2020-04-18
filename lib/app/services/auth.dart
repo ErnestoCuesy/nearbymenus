10b09af99528e766e1c881c7edfdf9db17263b67
ernestocuesy <ercuesy@gmail.com>
2020-04-18 12:30:42 +0200

    Email verified flag no longer needed as it is no longer checked. Firebase does not trigger a change when emails are verified.

--- lib/app/services/auth.dart
+++ lib/app/services/auth.dart
@@ -3,11 +3,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 class UserAuth {
-  UserAuth({@required this.uid, @required this.photoUrl, @required this.displayName, @required this.isEmailVerified});
+  UserAuth({
+    @required this.uid,
+    @required this.photoUrl,
+    @required this.displayName,
+  });
   final String uid;
   final String photoUrl;
   final String displayName;
-  final bool isEmailVerified;
 }
 
 abstract class AuthBase {
@@ -24,7 +27,13 @@ class Auth implements AuthBase {
 
   UserAuth _userFromFirebase(FirebaseUser user) {
     print('FirebaseUser => ${user.displayName}');
-    return user == null ? null : UserAuth(uid: user.uid, displayName: user.displayName, photoUrl: user.photoUrl, isEmailVerified: user.isEmailVerified);
+    return user == null
+        ? null
+        : UserAuth(
+            uid: user.uid,
+            displayName: user.displayName,
+            photoUrl: user.photoUrl,
+          );
   }
 
   @override
@@ -39,36 +48,40 @@ class Auth implements AuthBase {
   }
 
   @override
-  Future<UserAuth> signInWithEmailAndPassword(String email, String password) async {
-    final authResult = await _fireBaseAuth.signInWithEmailAndPassword(email: email, password: password);
+  Future<UserAuth> signInWithEmailAndPassword(
+      String email, String password) async {
+    final authResult = await _fireBaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
     if (!authResult.user.isEmailVerified) {
       throw PlatformException(
-        code: 'ERROR_EMAIL_NOT_VERIFIED',
-        message: 'Please verify your email address by clicking on the link emailed to you.'
-      );
+          code: 'ERROR_EMAIL_NOT_VERIFIED',
+          message:
+              'Please verify your email address by clicking on the link emailed to you.');
     } else {
       return _userFromFirebase(authResult.user);
     }
   }
 
   @override
-  Future<void> createUserWithEmailAndPassword(String email, String password) async {
-    final authResult = await _fireBaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+  Future<void> createUserWithEmailAndPassword(
+      String email, String password) async {
+    final authResult = await _fireBaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
     // TODO Hotmail filters verification email sent by Firebase
     authResult.user.sendEmailVerification();
     throw PlatformException(
         code: 'EMAIL_NOT_VERIFIED',
-        message: 'Please verify your email address by clicking on the link emailed to you.'
-    );
+        message:
+            'Please verify your email address by clicking on the link emailed to you.');
   }
 
   @override
   Future<void> resetPassword(String email) async {
     await _fireBaseAuth.sendPasswordResetEmail(email: email);
     throw PlatformException(
-      code: 'PASSWORD_RESET',
-      message: 'A password reset link has been emailed to you. Please click on it to enter your new password and try to sign in again.'
-    );
+        code: 'PASSWORD_RESET',
+        message:
+            'A password reset link has been emailed to you. Please click on it to enter your new password and try to sign in again.');
   }
 
   @override
