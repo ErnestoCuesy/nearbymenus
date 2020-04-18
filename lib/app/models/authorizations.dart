ebc2eda621f5191dcae405554fb82c48b2507da8
ernestocuesy <ercuesy@gmail.com>
2020-04-18 18:33:01 +0200

    Added id to authorizations.

--- lib/app/models/authorizations.dart
+++ lib/app/models/authorizations.dart
@@ -1,14 +1,16 @@
 class Authorizations {
+  final String id;
   final Map<String, dynamic> authorizedRoles;
   final Map<String, dynamic> authorizedNames;
 
-  Authorizations({this.authorizedRoles, this.authorizedNames});
+  Authorizations({this.id, this.authorizedRoles, this.authorizedNames});
 
   factory Authorizations.fromMap(Map<dynamic, dynamic> value, String documentId) {
     if (value == null) {
       return null;
     }
     return Authorizations(
+      id: documentId,
       authorizedRoles: value['authorizedRoles'],
       authorizedNames: value['authorizedNames'],
     );
