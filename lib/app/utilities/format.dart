c48823565f6ccfc6ea27f6f51acb91285a8b49ec
ernestocuesy <ercuesy@gmail.com>
2020-04-19 17:05:53 +0200

    Added formatDateTime method.

--- lib/app/utilities/format.dart
+++ lib/app/utilities/format.dart
@@ -24,4 +24,11 @@ class Format {
     }
     return '';
   }
+
+  static String formatDateTime(int date) {
+    String dateTimeIn = DateTime.fromMillisecondsSinceEpoch(date).toString();
+    List<String> splitTime = dateTimeIn.split('.');
+    return splitTime[0];
+  }
+
 }
