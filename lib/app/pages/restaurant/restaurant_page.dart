05a819271335087d23a10f5fbc4bed2f99d626fe
ernestocuesy <ercuesy@gmail.com>
2020-04-21 12:46:02 +0200

    Added support to delete restaurants.

--- lib/app/pages/restaurant/restaurant_page.dart
+++ lib/app/pages/restaurant/restaurant_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nearbymenus/app/common_widgets/list_items_builder.dart';
+import 'package:nearbymenus/app/common_widgets/platform_alert_dialog.dart';
+import 'package:nearbymenus/app/common_widgets/platform_exception_alert_dialog.dart';
 import 'package:nearbymenus/app/models/restaurant.dart';
 import 'package:nearbymenus/app/models/session.dart';
 import 'package:nearbymenus/app/pages/restaurant/restaurant_details_page.dart';
@@ -29,6 +32,35 @@ class _RestaurantPageState extends State<RestaurantPage> {
     );
   }
 
+  Future<void> _deleteRestaurant(BuildContext context, Restaurant restaurant) async {
+    try {
+      await database.deleteRestaurant(restaurant);
+    } on PlatformException catch (e) {
+      PlatformExceptionAlertDialog(
+        title: 'Operation failed',
+        exception: e,
+      ).show(context);
+    }
+  }
+
+  Future<bool> _confirmDismiss(BuildContext context, Restaurant restaurant) async {
+    if (restaurant.acceptingStaffRequests || restaurant.active || restaurant.open) {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Disable restaurant status flags first to delete'),
+        ),
+      );
+      return false;
+    } else {
+      return await PlatformAlertDialog(
+        title: 'Confirm restaurant deletion',
+        content: 'Do you really want to delete this restaurant? Menus and authorizations will also be deleted.',
+        cancelActionText: 'No',
+        defaultActionText: 'Yes',
+      ).show(context);
+    }
+  }
+
   Widget _buildContents(BuildContext context) {
     return StreamBuilder<List<Restaurant>>(
       stream: database.managerRestaurants(database.userId),
@@ -36,43 +68,50 @@ class _RestaurantPageState extends State<RestaurantPage> {
         return ListItemsBuilder<Restaurant>(
           snapshot: snapshot,
           itemBuilder: (context, restaurant) {
-            return Card(
-              margin: EdgeInsets.all(12.0),
-              child: ListTile(
-                isThreeLine: true,
-                leading: Icon(Icons.restaurant),
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                        restaurant.name,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    Text(restaurant.restaurantLocation),
-                  ],
+            return Dismissible(
+              background: Container(color: Colors.red),
+              key: Key('res-${restaurant.id}'),
+              direction: DismissDirection.endToStart,
+              confirmDismiss: (_) => _confirmDismiss(context, restaurant),
+              onDismissed: (direction) => _deleteRestaurant(context, restaurant),
+              child: Card(
+                margin: EdgeInsets.all(12.0),
+                child: ListTile(
+                  isThreeLine: true,
+                  leading: Icon(Icons.restaurant),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                          restaurant.name,
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Text(restaurant.restaurantLocation),
+                    ],
+                  ),
+                  // subtitle: Text('${restaurant.restaurantLocation}'),
+                  subtitle: Column(
+                    children: <Widget>[
+                      CheckboxListTile(
+                        title: Text('Listing is active'),
+                        value: restaurant.active,
+                        onChanged: null,
+                      ),
+                      CheckboxListTile(
+                        title: Text('Restaurant shows as open'),
+                        value: restaurant.open,
+                        onChanged: null,
+                      ),
+                      CheckboxListTile(
+                        title: Text('Accepting staff requests'),
+                        value: restaurant.acceptingStaffRequests,
+                        onChanged: null,
+                      ),
+                    ],
+                  ),
+                  trailing: Icon(Icons.edit),
+                  onTap: () => _createRestaurantDetailsPage(context, restaurant),
                 ),
-                // subtitle: Text('${restaurant.restaurantLocation}'),
-                subtitle: Column(
-                  children: <Widget>[
-                    CheckboxListTile(
-                      title: Text('Listing is active'),
-                      value: restaurant.active,
-                      onChanged: null,
-                    ),
-                    CheckboxListTile(
-                      title: Text('Restaurant shows as open'),
-                      value: restaurant.open,
-                      onChanged: null,
-                    ),
-                    CheckboxListTile(
-                      title: Text('Accepting staff requests'),
-                      value: restaurant.acceptingStaffRequests,
-                      onChanged: null,
-                    ),
-                  ],
-                ),
-                trailing: Icon(Icons.edit),
-                onTap: () => _createRestaurantDetailsPage(context, restaurant),
               ),
             );
           }
