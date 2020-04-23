d79c8e13311fafc59845eabbd53854db8b98b2c8
ernestocuesy <ercuesy@gmail.com>
2020-04-23 17:01:05 +0200

    Some renaming

--- lib/app/models/order.dart
+++ lib/app/models/order.dart
@@ -1,5 +1,5 @@
 import 'package:meta/meta.dart';
-import 'package:nearbymenus/app/models/menu_item.dart';
+import 'package:nearbymenus/app/models/item.dart';
 import 'package:nearbymenus/app/services/auth.dart';
 import 'address.dart';
 
@@ -7,7 +7,7 @@ class Order {
   final int orderNumber;
   final UserAuth user;
   final Address deliveryAddress;
-  final List<MenuItem> orderItems;
+  final List<Item> orderItems;
 
   Order(this.orderNumber, this.user, this.deliveryAddress, this.orderItems);
 }
