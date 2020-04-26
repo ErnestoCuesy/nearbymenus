c90295f90074bcea8e758b6f8e33addbc0eaa3f8
ernestocuesy <ercuesy@gmail.com>
2020-04-26 18:33:39 +0200

    Added extra flags to section to be able to group sides and extras and display or hide on the patron's menu.

--- lib/app/models/section.dart
+++ lib/app/models/section.dart
@@ -2,8 +2,20 @@ class Section {
   final String id;
   final String menuId;
   final String name;
+  final String notes;
+  final bool hidden;
+  final bool onlyForExtras;
+  final bool onlyForSides;
 
-  Section({this.id, this.menuId, this.name});
+  Section(
+      {this.id,
+      this.menuId,
+      this.name,
+      this.notes,
+      this.hidden,
+      this.onlyForExtras,
+      this.onlyForSides,
+  });
 
   factory Section.fromMap(Map<String, dynamic> data, String documentID) {
     if (data == null) {
@@ -13,6 +25,10 @@ class Section {
       id: data['id'],
       menuId: data['menuId'],
       name: data['name'],
+      notes: data['notes'],
+      hidden: data['hidden'],
+      onlyForExtras: data['onlyForExtras'],
+      onlyForSides: data['onlyForSides'],
     );
   }
 
@@ -21,12 +37,15 @@ class Section {
       'id': id,
       'menuId': menuId,
       'name': name,
+      'notes': notes,
+      'hidden': hidden,
+      'onlyForExtras': onlyForExtras,
+      'onlyForSides': onlyForSides,
     };
   }
 
   @override
   String toString() {
-    return 'id: $id, menuId: $menuId, name: $name';
+    return 'id: $id, menuId: $menuId, name: $name, notes: $notes, hidden: $hidden, onlyForExtras: $onlyForExtras, onlyForSides: $onlyForSides,';
   }
-
 }
