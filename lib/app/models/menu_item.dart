5a3720de8e5fffeb119c7f61f68a057076ecb7a9
ernestocuesy <ercuesy@gmail.com>
2020-05-06 15:14:36 +0200

    Added support for item menu options and variations.

--- lib/app/models/menu_item.dart
+++ lib/app/models/menu_item.dart
@@ -1,16 +1,56 @@
-import 'package:meta/meta.dart';
-
-import 'discount.dart';
-import 'ingredient_options.dart';
-import 'menu_category.dart';
-
 class MenuItem {
+  final String id;
+  final String menuId;
   final String name;
-  final MenuCategory category;
   final String description;
-  final List<IngredientOptions> ingredientVariations;
   final double price;
-  final List<Discount> discounts;
+  final List<String> options;
+  final bool isExtra;
+  final bool isSide;
 
-  MenuItem(this.name, this.category, this.description, this.ingredientVariations, this.price, this.discounts);
+  MenuItem({
+    this.id,
+    this.menuId,
+    this.name,
+    this.description,
+    this.price,
+    this.options,
+    this.isExtra,
+    this.isSide,
+  });
+
+  factory MenuItem.fromMap(Map<String, dynamic> data, String documentID) {
+    if (data == null) {
+      return null;
+    }
+    return MenuItem(
+      id: data['id'],
+      menuId: data['menuId'],
+      name: data['name'],
+      description: data['description'],
+      price: data['price'],
+      options: List.from(data['options']) ?? [],
+      isExtra: data['isExtra'],
+      isSide: data['isSide'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'menuId': menuId,
+      'name': name,
+      'description': description,
+      'price': price,
+      'options': options,
+      'isExtra': isExtra,
+      'isSide': isSide,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'id: $id, menuId: $menuId, name: $name, description: $description, price: $price, options: $options, isExtra: $isExtra, isSide: $isSide';
+  }
+
 }
