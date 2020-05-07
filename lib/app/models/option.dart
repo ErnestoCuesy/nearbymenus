e71112e4af0f901fb4d418b2ba4940c396e171f7
ernestocuesy <ercuesy@gmail.com>
2020-05-07 14:18:30 +0200

    Added array to track items that use this option. This is to ensure that there are no menu items with options that no longer exist and also to prevent options from being deleted when they're still used by menu items. User will be warned before deletion.

--- lib/app/models/option.dart
+++ lib/app/models/option.dart
@@ -3,23 +3,32 @@ class Option {
   final String restaurantId;
   final String name;
   final int numberAllowed;
+  List<dynamic> usedByMenuItems;
 
   Option({
     this.id,
     this.restaurantId,
     this.name,
     this.numberAllowed,
+    this.usedByMenuItems,
   });
 
   factory Option.fromMap(Map<String, dynamic> data, String documentID) {
     if (data == null) {
       return null;
     }
+    List<String> usedByMenuItems;
+    if (data['usedByMenuItems'] != null) {
+      usedByMenuItems = List.from(data['usedByMenuItems']);
+    } else {
+      usedByMenuItems = List<String>();
+    }
     return Option(
       id: data['id'],
       restaurantId: data['restaurantId'],
       name: data['name'],
       numberAllowed: data['numberAllowed'],
+      usedByMenuItems: usedByMenuItems,
     );
   }
 
@@ -29,11 +38,12 @@ class Option {
       'restaurantId': restaurantId,
       'name': name,
       'numberAllowed': numberAllowed,
+      'usedByMenuItems': usedByMenuItems ?? [],
     };
   }
 
   @override
   String toString() {
-    return 'id: $id, restaurantId: $restaurantId, name: $name, numberAllowed: $numberAllowed';
+    return 'id: $id, restaurantId: $restaurantId, name: $name, numberAllowed: $numberAllowed, usedByMenuItems: $usedByMenuItems';
   }
 }
