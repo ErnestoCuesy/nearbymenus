8c51c053e95346c5cffb1491371eb7732555f25a
ernestocuesy <ercuesy@gmail.com>
2020-05-10 17:04:55 +0200

    Added validation of option rules before placing order in cart.

--- lib/app/pages/orders/add_to_order.dart
+++ lib/app/pages/orders/add_to_order.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 import 'package:nearbymenus/app/common_widgets/form_submit_button.dart';
+import 'package:nearbymenus/app/common_widgets/platform_exception_alert_dialog.dart';
 import 'package:nearbymenus/app/models/order.dart';
 import 'package:nearbymenus/app/models/order_item.dart';
 import 'package:nearbymenus/app/models/session.dart';
@@ -25,6 +27,7 @@ class _AddToOrderState extends State<AddToOrder> {
   List<String> itemKeys = List<String>();
   Map<String, dynamic> get item => widget.item;
   List<String> menuItemOptions = List<String>();
+  Map<String, int> optionsSelectionCounters = Map<String, int>();
 
   void _addMenuItemToOrder() {
     final double timestamp = dateFromCurrentDate() / 1.0;
@@ -64,6 +67,7 @@ class _AddToOrderState extends State<AddToOrder> {
       child: Padding(
         padding: const EdgeInsets.all(24.0),
         child: Container(
+          width: MediaQuery.of(context).size.width - 24.0,
           color: Theme.of(context).dialogBackgroundColor,
           child: Padding(
             padding: const EdgeInsets.all(16.0),
@@ -80,9 +84,12 @@ class _AddToOrderState extends State<AddToOrder> {
                   SizedBox(
                     height: 16.0,
                   ),
-                  Text(
-                    '${widget.item['description']}',
-                    //style: Theme.of(context).accentTextTheme.headline5,
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                    child: Text(
+                      '${widget.item['description']}',
+                      //style: Theme.of(context).accentTextTheme.headline5,
+                    ),
                   ),
                   SizedBox(
                     height: 16.0,
@@ -104,9 +111,20 @@ class _AddToOrderState extends State<AddToOrder> {
                     context: context,
                     text: 'Add to order',
                     color: Theme.of(context).primaryColor,
-                    onPressed: () {
-                      _addMenuItemToOrder();
-                      Navigator.of(context).pop();
+                    onPressed: () async {
+                      if (_optionsAreValid()) {
+                        _addMenuItemToOrder();
+                        Navigator.of(context).pop();
+                      } else {
+                        await PlatformExceptionAlertDialog(
+                          title: 'Incorrect number of options selected',
+                          exception: PlatformException(
+                            code: 'INCORRECT_OPTIONS',
+                            message:  'Please check your choices.',
+                            details:  'Please check your choices.',
+                          ),
+                        ).show(context);
+                      }
                     },
                   ),
                   SizedBox(
@@ -119,6 +137,26 @@ class _AddToOrderState extends State<AddToOrder> {
         ),
       ),
     );
+  }
+
+  bool _optionsAreValid() {
+    if (widget.item['options'].isEmpty) {
+      return true;
+    }
+    if (optionsSelectionCounters.isEmpty) {
+      return false;
+    }
+    bool optionsAreValid = true;
+    widget.item['options'].forEach((key) {
+      Map<String, dynamic> optionValue = widget.options[key];
+      final maxAllowed = optionValue['numberAllowed'];
+      if (optionsSelectionCounters[optionValue['name']] == null ||
+          optionsSelectionCounters[optionValue['name']] > maxAllowed ||
+          optionsSelectionCounters[optionValue['name']] == 0) {
+        optionsAreValid = false;
+      }
+    });
+    return optionsAreValid;
   }
 
   List<Widget> _buildOptions() {
@@ -147,13 +185,13 @@ class _AddToOrderState extends State<AddToOrder> {
       );
       optionValue.forEach((key, value) {
         if (key.toString().length > 20) {
-          optionList.add(CheckboxListTile(
-            title: Text(
-              '${value['name']}',
+            optionList.add(CheckboxListTile(
+              title: Text(
+                '${value['name']}',
+              ),
+              value: optionCheck('${optionValue['name']}: ${value['name']}'),
+              onChanged: (addFlag) => _updateOptionsList(optionValue['name'], '${optionValue['name']}: ${value['name']}', addFlag),
             ),
-            value: optionCheck('${optionValue['name']}: ${value['name']}'),
-            onChanged: (addFlag) => _updateOptionsList('${optionValue['name']}: ${value['name']}', addFlag),
-          ),
           );
         }
       });
@@ -161,13 +199,22 @@ class _AddToOrderState extends State<AddToOrder> {
     return optionList;
   }
 
-  void _updateOptionsList(String option, bool addFlag) {
-    menuItemOptions.clear();
+  void _updateOptionsList(String key, String option, bool addFlag) {
     setState(() {
       if (addFlag) {
         menuItemOptions.add(option);
+        if (optionsSelectionCounters.containsKey(key)) {
+          optionsSelectionCounters.update(key, (value) => value + 1);
+        } else {
+          optionsSelectionCounters.putIfAbsent(key, () => 1);
+        }
       } else {
         menuItemOptions.remove(option);
+        if (optionsSelectionCounters.containsKey(key)) {
+          optionsSelectionCounters.update(key, (value) => value - 1);
+        } else {
+          optionsSelectionCounters.putIfAbsent(key, () => 0);
+        }
       }
     });
   }
