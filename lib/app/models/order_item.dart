907c821a0d8a5a550c5eca12c40ab783ee890dc0
ernestocuesy <ercuesy@gmail.com>
2020-05-16 10:43:58 +0200

    Added menu code.

--- lib/app/models/order_item.dart
+++ lib/app/models/order_item.dart
@@ -1,13 +1,14 @@
 class OrderItem {
   final String id;
   final String orderId;
+  final String menuCode;
   final String name;
   final int quantity;
   final double price;
   final double lineTotal;
   final List<String> options;
 
-  OrderItem({this.id, this.orderId, this.name, this.quantity, this.options, this.price, this.lineTotal});
+  OrderItem({this.id, this.orderId, this.menuCode, this.name, this.quantity, this.options, this.price, this.lineTotal});
 
   factory OrderItem.fromMap(Map<String, dynamic> data, String documentID) {
     if (data == null) {
@@ -22,6 +23,7 @@ class OrderItem {
     return OrderItem(
       id: data['id'],
       orderId: data['orderId'],
+      menuCode: data['menuCode'],
       name: data['name'],
       quantity: data['quantity'],
       price: data['price'],
@@ -34,6 +36,7 @@ class OrderItem {
     return {
       'id': id,
       'orderId': orderId,
+      'menuCode': menuCode,
       'name': name,
       'quantity': quantity,
       'price': price,
@@ -44,7 +47,7 @@ class OrderItem {
 
   @override
   String toString() {
-    return 'id: $id, orderId: $orderId, name: $name, quantity: $quantity, price: $price, lineTotal: $lineTotal, options: $options';
+    return 'id: $id, orderId: $orderId, menuCode: $menuCode, name: $name, quantity: $quantity, price: $price, lineTotal: $lineTotal, options: $options';
   }
 
 }
