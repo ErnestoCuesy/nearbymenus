b5dcc8576d114c821ddac24eec3701e5f4e89179
ernestocuesy <ercuesy@gmail.com>
2020-05-18 16:04:54 +0200

    Changed from subscription model to order bundles and refactored accordingly. Still WIP.

--- lib/app/common_widgets/purchase_button.dart
+++ lib/app/common_widgets/purchase_button.dart
@@ -1,40 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nearbymenus/app/common_widgets/custom_raised_button.dart';
-import 'package:nearbymenus/app/models/session.dart';
-import 'package:nearbymenus/app/models/user_details.dart';
+import 'package:nearbymenus/app/common_widgets/platform_exception_alert_dialog.dart';
+import 'package:nearbymenus/app/models/bundle.dart';
 import 'package:nearbymenus/app/services/database.dart';
 import 'package:nearbymenus/app/services/iap_manager.dart';
-import 'package:provider/provider.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 
 class PurchaseButton extends StatelessWidget {
+  final IAPManagerBase iap;
+  final Database database;
   final Package package;
 
-  PurchaseButton({Key key, @required this.package}) : super(key: key);
+  PurchaseButton({Key key, @required this.iap, this.database, this.package}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final iap = Provider.of<IAPManagerBase>(context, listen: false);
-    final database = Provider.of<Database>(context);
-    final session = Provider.of<Session>(context);
-    return CustomRaisedButton(
-      height: 150.0,
-      width: 250.0,
-      color: Theme.of(context).buttonTheme.colorScheme.background,
-      onPressed: () async {
-        iap.purchasePackage(package);
-        var userDetails = session.userDetails;
-        userDetails.role = ROLE_MANAGER;
-        database.setUserDetails(userDetails);
-        Navigator.of(context).pop();
-        // TODO wrap in try/catch for server errors
-      },
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Text("${package.product.description}", style: Theme.of(context).accentTextTheme.title,),
-          Text("Buy - (${package.product.priceString})", style: Theme.of(context).accentTextTheme.title),
-        ],
+    return Builder(
+      builder: (BuildContext context) => CustomRaisedButton(
+        height: 150.0,
+        width: 250.0,
+        color: Theme.of(context).buttonTheme.colorScheme.background,
+        onPressed: () async {
+          try {
+            iap.purchasePackage(package);
+            int testBundles = 5;
+            final bundleUpdateDate = documentIdFromCurrentDate();
+            await database.setBundleCounterTransaction(database.userId, testBundles);
+            database.setBundle(database.userId, Bundle(
+                      id: bundleUpdateDate,
+                      purchaseDate: bundleUpdateDate,
+                      rcInfo: 'RevenueCat stuff',
+                      ordersInBundle: testBundles
+                    ));
+            await PlatformExceptionAlertDialog(
+                title: 'Thank you!',
+                exception: PlatformException(
+                code: 'ORDER_BUNDLED_PURCHASE_SUCCESS',
+                message:  'Your purchase was successful. You can unlock your orders.',
+                details:  'Your purchase was successful. You can unlock your orders.',
+              ),
+            ).show(context);
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              "${package.product.description}",
+              style: Theme.of(context).accentTextTheme.headline6,
+            ),
+            Text(
+                "Buy - (${package.product.priceString})",
+                style: Theme.of(context).accentTextTheme.headline6,
+            ),
+          ],
+        ),
       ),
     );
   }
