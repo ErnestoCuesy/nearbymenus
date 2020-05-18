f4f61b8ed1b4cffe0bf5869b551afcad18c79ce4
ernestocuesy <ercuesy@gmail.com>
2020-05-18 16:02:54 +0200

    Changed from subscription model to order bundles and refactored accordingly. Still WIP.

--- lib/app/pages/session/upsell_screen.dart
+++ lib/app/pages/session/upsell_screen.dart
@@ -1,45 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:nearbymenus/app/common_widgets/purchase_button.dart';
+import 'package:nearbymenus/app/models/session.dart';
+import 'package:nearbymenus/app/services/database.dart';
 import 'package:nearbymenus/app/services/iap_manager.dart';
+import 'package:provider/provider.dart';
 
-class UpsellScreen extends StatelessWidget {
-  final Subscription subscription;
+class UpsellScreen extends StatefulWidget {
+  final Database database;
+  final Session session;
+  final int ordersLeft;
+  final int ordersBlocked;
 
-  const UpsellScreen({Key key, this.subscription}) : super(key: key);
+  const UpsellScreen({Key key, this.database, this.session, this.ordersLeft, this.ordersBlocked}) : super(key: key);
 
-  List<Widget> buildPackages() {
+  @override
+  _UpsellScreenState createState() => _UpsellScreenState();
+}
+
+class _UpsellScreenState extends State<UpsellScreen> {
+  IAPManagerBase iap;
+
+  Database get database => widget.database;
+  Session get session => widget.session;
+  int get ordersLeft => widget.ordersLeft;
+  int get ordersBlocked => widget.ordersBlocked;
+
+  List<Widget> buildPackages(BuildContext context) {
+    print('Orders left: $ordersLeft');
     List<Widget> packages = List<Widget>();
-    if (subscription.subscriptionType == SubscriptionType.Unsubscribed) {
-      subscription.availableOfferings.forEach((pkg) {
-        packages.add(PurchaseButton(
-          package: pkg,
-        ));
-        packages.add(SizedBox(height: 24.0,));
-      });
-    } else {
+    if (ordersLeft != null) {
       packages.add(Text(
-          'Active subscriptions: ${subscription.numberOfActiveSubscriptions}'));
-      packages.add(Text('First seen: ${subscription.firstSeen}'));
-      packages.add(
-          Text('Latest expiration date: ${subscription.latestExpirationDate}'));
+        'Orders left: ${ordersLeft.toString()}',
+        style: Theme
+            .of(context)
+            .textTheme
+            .headline4,
+      ));
+      packages.add(SizedBox(height: 16.0,));
     }
+    if (ordersBlocked != null) {
+      packages.add(Text(
+        'Orders locked: ${widget.ordersBlocked}',
+        style: Theme
+            .of(context)
+            .textTheme
+            .headline4,
+      ));
+      packages.add(SizedBox(height: 16.0,));
+    }
+    session.subscription.availableOfferings.forEach((pkg) {
+      packages.add(PurchaseButton(
+        iap: iap,
+        database: database,
+        package: pkg,
+      ));
+      packages.add(SizedBox(height: 24.0,));
+    });
     return packages;
   }
 
   @override
   Widget build(BuildContext context) {
+    iap = Provider.of<IAPManagerBase>(context, listen: false);
     return Scaffold(
         appBar: AppBar(
           title: Text(
-            subscription.subscriptionType == SubscriptionType.Unsubscribed
-                ? "Available Packages"
-                : "Subscription Details",
+            'Buy a bundle to unlock your orders',
           ),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: buildPackages(),
+            children: buildPackages(context),
           ),
         ));
   }
