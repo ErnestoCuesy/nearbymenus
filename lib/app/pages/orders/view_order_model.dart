2a370cf230ac1de53517a81a60e4c6ce3528bfc4
ernestocuesy <ercuesy@gmail.com>
2020-05-19 14:39:15 +0200

    This logic split into view - model pattern for optimal architecture and nicer UX.

---
+++ lib/app/pages/orders/view_order_model.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:nearbymenus/app/models/order.dart';
+import 'package:nearbymenus/app/models/session.dart';
+import 'package:nearbymenus/app/services/database.dart';
+
+class ViewOrderModel with ChangeNotifier {
+  final Database database;
+  final Session session;
+  Order order;
+  bool isLoading;
+  bool submitted;
+
+  ViewOrderModel(
+      {@required this.database,
+        @required this.session,
+        @required this.order,
+        this.isLoading = false,
+        this.submitted = false,
+      });
+
+  Future<void> save() async {
+    updateWith(isLoading: true, submitted: true);
+    _submitOrder();
+  }
+
+  Future<void> _submitOrder() async {
+    try {
+      order.status = ORDER_PLACED;
+      database.setOrderTransaction(session.nearestRestaurant.managerId,
+          session.nearestRestaurant.id,
+          order);
+      session.currentOrder = null;
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+
+  void updateNotes(String notes) => updateWith(notes: notes);
+
+  void deleteOrderItem(int index) {
+      order.orderItems.removeAt(index);
+      notifyListeners();
+  }
+  
+  String get primaryButtonText => 'Save';
+
+  bool get canSave => _checkOrder();
+
+  bool _checkOrder() {
+    return order.paymentMethod != '' && order.orderTotal > 0;
+  }
+
+  void updatePaymentMethod(String key, bool flag) {
+    if (order.status != ORDER_ON_HOLD) {
+      return;
+    }
+    if (flag) {
+      updateWith(paymentMethod: key);
+    } else {
+      updateWith(paymentMethod: '');
+    }
+  }
+
+  bool optionCheck(String key) => order.paymentMethod == key;
+
+  void updateWith({
+    String notes,
+    String paymentMethod,
+    bool isLoading,
+    bool submitted,
+  }) {
+    this.order.notes = notes ?? this.order.notes;
+    this.order.paymentMethod = paymentMethod ?? this.order.paymentMethod;
+    this.isLoading = isLoading ?? this.isLoading;
+    this.submitted = this.submitted;
+    notifyListeners();
+  }
+}
