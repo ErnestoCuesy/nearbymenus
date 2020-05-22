40d24499ff22e40a3b83b0231e90ece70a92194e
ernestocuesy <ercuesy@gmail.com>
2020-05-22 13:30:24 +0200

    Refactored for flavors.

--- lib/app/pages/home/home_page_manager.dart
+++ lib/app/pages/home/home_page_manager.dart
@@ -26,7 +26,7 @@ class _HomePageManagerState extends State<HomePageManager> {
   Database database;
 
   String get role => widget.role;
-  TabItem _currentTab = TabItem.userAccount;
+  TabItem _currentTab = TabItem.restaurant;
 
   final Map<TabItem, GlobalKey<NavigatorState>> navigatorKeys = {
     TabItem.restaurant: GlobalKey<NavigatorState>(),
