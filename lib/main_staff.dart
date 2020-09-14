ab3e3275f71777707a7af179738151082b7898a7
ernestocuesy <ercuesy@gmail.com>
2020-09-14 12:52:11 +0200

    Changes to initialize Firebase the right way.

--- lib/main_staff.dart
+++ lib/main_staff.dart
@@ -1,9 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:nearbymenus/app/init_firebase.dart';
 import 'app/config/flavour_config.dart';
-import 'app/app.dart';
 
-void main() async {
+void main() {
   var flavour = Flavour.STAFF;
   FlavourConfig(
     flavour: flavour,
@@ -12,11 +11,6 @@ void main() async {
   );
 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
 
-  runApp(
-    MaterialApp(
-      home: MyApp(),
-    ),
-  );
+  runApp(InitFirebase());
 }
